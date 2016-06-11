77a66e0bc0cd62d39a061190ad25a2fc974ca93d
mwamth <postester2015@gmail.com>
2016-06-11 21:18:50 +0900

    参照処理追加

---
+++ htdocs/api/event.php
@@ -0,0 +1,25 @@
+<?php
+  //POSTデータ処理
+  if ($_SERVER['REQUEST_METHOD']==='GET'){
+    $event_id = $_GET['id'];
+  }
+  //DB関連
+  require_once("./send_sql.php");
+  $sql_event = "SELECT * FROM event WHERE event_id = $event_id";
+  $res_event = send_sql($sql_event);
+
+?>
+
+<html>
+<head>
+<meta http-equiv=content-type content="text/html; charset=UTF-8">
+<title>調整ちゃん</title>
+</head>
+<body>
+  <?php
+  while ($event = mysql_fetch_array($res_event) {
+		print $event['event_name']." ".$event['comment']."<br>";
+	}
+  ?>
+</body>
+</html>
