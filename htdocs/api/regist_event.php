170546f6ff08930510af3b9e08cffc05e153e935
x4066x <x4066x@gmail.com>
2016-09-11 12:59:09 +0900

    「日時」登録のためのDB登録方法変更
    item.candi_idへの登録方法を変更して、
    itemは一行ごとに、
    candi_idは０からの連番で登録できるようになった
    
    次は、candi_idとevent_idをもとにitemを引っ張ってくる、表示の処理をevent.phpに書くこと

--- htdocs/api/regist_event.php
+++ htdocs/api/regist_event.php
@@ -1,4 +1,5 @@
 <?php
+
   //POSTデータ処理
   if ($_SERVER['REQUEST_METHOD']==='POST'){
     $event_name = $_POST['event_name'];
@@ -8,21 +9,28 @@
   }
   //DB関連
   require_once("./Pdodb.php");
-  $event_id = md5(uniqid(rand()));
-  var_dump($event_id);
-
-  $sql_event = "INSERT INTO ccdb.event(event_name, place, comment,event_id) VALUES('$event_name','$place','$comment','$event_id')";
-
-  $sql_candi = "INSERT INTO ccdb.candi(item, candi_id, event_id) VALUES('$item','36','$event_id')";
-
   //インスタンス生成
   $db = new Pdodb();
+  //イベントID ランダム生成
+  $event_id = md5(uniqid(rand()));
 
-  //イベントデータ挿入
+//日時処理
+  $cr = array("\r\n","\r");
+  $item = str_replace($cr,"\n",$item);
+  $item_array = explode("\n",$item);
+  $item_array = array_filter($item_array,'strlen');
+  $item_array = array_values($item_array);
+  foreach ($item_array as $key => $value){
+    $sql_candi = "INSERT INTO ccdb.candi(item, candi_id, event_id) VALUES('$value','$key','$event_id')";
+    $db->SendSql($sql_candi);
+  }
+
+
+//イベント名、場所、コメント処理
+    $sql_event = "INSERT INTO ccdb.event(event_name, place, comment,event_id) VALUES('$event_name','$place','$comment','$event_id')";
+
+//イベントデータ挿入
   $db->SendSql($sql_event);
-
-  //候補データ挿入
-  $db->SendSql($sql_candi);
 
   $url = './event.php'.'?id='.$event_id;
   header("Location: {$url}");
